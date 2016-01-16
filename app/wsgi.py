0fd911ddd773b550a81cd3c0a009191243ab1d10
Salar Rahmanian <salar@softinio.com>
2016-01-16 16:34:09 -0500

    Initial Version

---
+++ app/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for valentino project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+from mezzanine.utils.conf import real_project_name
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "%s.settings" % real_project_name("app"))
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
